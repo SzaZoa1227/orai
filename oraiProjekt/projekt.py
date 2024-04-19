4b8bb3a90a1384e6abf51db80d4dc836a4ff7d8a
SzaZoa1227 <szabozoard27@gmail.com>
2024-04-19 11:35:24 +0200

    4.es feladat kesz

--- oraiProjekt/projekt.py
+++ oraiProjekt/projekt.py
@@ -1,7 +1,7 @@
 #otodik feladatig!! +8as
 
 class Adat:
-    def __init__(self, datum:str,nagydij: str, helyezes:int, befejezett_kor:int, pont:int, konstruktor:str, celba_ert:bool, hiba_oka: str) -> None:
+    def __init__(self, datum:str,nagydij: str, helyezes:int, befejezett_kor:int, pont:int, konstruktor:str, celba_ert:bool, korhatrany:int, hiba_oka: str) -> None:
         self.datum = datum
         self.nagydij = nagydij
         self.helyezes = helyezes
@@ -9,6 +9,7 @@ class Adat:
         self.pont = pont
         self.konstruktor = konstruktor
         self.celba_ert = celba_ert
+        self.korhatrany = korhatrany
         self.hiba_oka = hiba_oka
         pass
 adatok = []
@@ -20,11 +21,14 @@ with open("kimi.csv","rt",encoding="utf-8") as be:
         sor[3] = int(sor[3])
         sor[4] = int(sor[4])
         sor[6] = True if sor[6] == "I" else False
-        adatok.append(Adat(sor[0],sor[1],sor[2],sor[3],sor[4],sor[5],sor[6],sor[7]))
+        adatok.append(Adat(sor[0],sor[1],sor[2],sor[3],sor[4],sor[5],sor[6],sor[7],sor[8]))
 
 
 print(f"3. feladat: {len(adatok)}")
-print(f"4. feladat: Magyar Nagydíj helyezései\n")
+
+
+print(f"4. feladat: Magyar Nagydíj helyezései")
 for adat in adatok:
     if adat.nagydij == "Magyar Nagydíj":
-        print(f"\t{adat.datum}: {adat.helyezes}")
+        print(f"\t{adat.datum}: {adat.helyezes}. hely")
+
